3d44b1c37d1def8f9ffdac7744baac26498087e9
Bryan Zavala <noexprtees@gmail.com>
2026-04-02 17:25:21 -0500

    task: write base controller and database configs

--- back_end/weatherBackend/weatherBackend/Program.cs
+++ back_end/weatherBackend/weatherBackend/Program.cs
@@ -1,3 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using weatherBackend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controller Configs
+
+builder.Services.AddControllers()
+  .AddJsonOptions(options =>
+  {
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+  });
+
+// Database Configs
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+  var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+  db.Database.Migrate();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
